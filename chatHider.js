96b6a5d0306f06da1dc92c774841db09c004b576
Ed Bulpitt <63399988+DevEdXYZ@users.noreply.github.com>
2023-05-15 22:32:51 +0100

    Inverted svg icon to white
    Inverted svg icon to white

--- chatHider.js
+++ chatHider.js
@@ -6,15 +6,18 @@ const eyeClosed = chrome.runtime.getURL('eye-closed.svg');
 const eyeIcon = document.createElement('img');
 eyeIcon.src = eyeOpen;
 eyeIcon.style.position = 'fixed';
-eyeIcon.style.top = '2.2vh';  // 2% of the viewport height
-eyeIcon.style.left = '11vw';  // 2% of the viewport width
+eyeIcon.style.top = '2vh';  
+eyeIcon.style.left = '11vw';  
 eyeIcon.style.cursor = 'pointer';
 eyeIcon.style.zIndex = 10000; // Ensure the eye icon is always on top
+
+// Apply the invert color filter to the eye icon
+eyeIcon.style.filter = 'invert(100%)';
 
 // Append the eye icon to the body
 document.body.appendChild(eyeIcon);
 
-// Define the chat container (replace with the actual selector)
+// Define the chat container 
 const chatContainer = document.querySelector('.dark.flex-shrink-0.overflow-x-hidden.bg-gray-900');
 
 // Toggle the chat visibility when the eye icon is clicked
