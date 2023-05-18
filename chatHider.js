4d822aaa984610da5a3e24517bc57dacd9d1393f
Ed Bulpitt <63399988+DevEdXYZ@users.noreply.github.com>
2023-05-18 02:21:39 +0100

    Updated chatHider.js to have slide animation

--- chatHider.js
+++ chatHider.js
@@ -20,13 +20,17 @@ document.body.appendChild(eyeIcon);
 // Define the chat container 
 const chatContainer = document.querySelector('.dark.flex-shrink-0.overflow-x-hidden.bg-gray-900');
 
+// Apply initial CSS transform and transition properties to the chat container
+chatContainer.style.transform = 'translateX(0)';
+chatContainer.style.transition = 'transform 0.5s ease-in-out';
+
 // Toggle the chat visibility when the eye icon is clicked
 eyeIcon.addEventListener('click', () => {
-    if (chatContainer.style.display === 'none') {
-        chatContainer.style.display = '';
-        eyeIcon.src = eyeOpen;
-    } else {
-        chatContainer.style.display = 'none';
+    if (chatContainer.style.transform === 'translateX(0px)') {
+        chatContainer.style.transform = 'translateX(-100%)';
         eyeIcon.src = eyeClosed;
+    } else {
+        chatContainer.style.transform = 'translateX(0)';
+        eyeIcon.src = eyeOpen;
     }
 });
